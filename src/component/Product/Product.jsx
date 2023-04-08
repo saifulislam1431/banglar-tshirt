9e9ad432e283cef4cfd9d14df1c22ce47d94bb55
Saiful Islam <sakibbhaiya143@gmail.com>
2023-04-09 00:01:41 +0600

    Data loaded in UI

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const Product = () => {
+const Product = ({singleData}) => {
+    const {name,img,price,seller,stock} = singleData;
     return (
-        <div>
-            
+        <div className='flex items-center justify-center mt-5'>
+            <div className='product-card'>
+                <div className='card border p-5 rounded-xl shadow-md border-purple-300 mb-4'>
+                    <img src={img ? img : 'Image Not Found'} alt={name}  className='w-64 rounded-lg  mx-auto h-64'/>
+                    <h1>{name}</h1>
+                    <div>
+                        <p>Price: {price}</p>
+                        <p>Stock: {stock}</p>
+                    </div>
+                    <p>Brand: {seller}</p>
+                    <button className='bg-purple-300 py-3 w-full mt-4 rounded-lg text-purple-950 hover:bg-purple-600 hover:text-white font-semibold'>Buy Now</button>
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Product from '../Product/Product';
 
 const Shop = () => {
+    const allData = useLoaderData();
+    console.log(allData);
     return (
         <div>
-            
+            <div className='grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-4 gap-5'>
+                {
+                    allData.map(singleData => <Product
+                    key={singleData.id}
+                    singleData={singleData}
+                    ></Product>)
+                }
+            </div>
         </div>
     );
 };
