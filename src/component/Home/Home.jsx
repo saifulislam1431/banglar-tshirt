17c6e4288746fe67b7250e1bdde1109c93da1f2a
Saiful Islam <sakibbhaiya143@gmail.com>
2023-04-08 23:20:12 +0600

    Home section added

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Lottie from "lottie-react";
 import clothe from '../../assets/108631-aloha-clothes.json';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div className='flex justify-center items-center mt-10'>
-            <div className='flex'>
+        <div className='min-h-[calc(100vh-112px)] flex justify-center items-center my-container'>
+            <div className='flex justify-center items-center gap-10'>
                 <div>
-                   <h1>Trendy is the last stage before tacky.</h1>
+                    <h1 className='text-4xl font-bold tracking-wide leading-relaxed text-purple-900'>Dress like you're already famous.</h1>
+                    <h1 className='text-xl font-semibold my-4 text-gray-700'>Trendy !s the last stage before tacky.</h1>
+                    <Link to='/shop'><button className='btn-shop'>Shop Now</button></Link>
                 </div>
                 <div>
                     <Lottie animationData={clothe} loop={true}></Lottie>
